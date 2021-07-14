eb8e84ae2cdcf9a99c342154b48b20c090d03328
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-07-14 15:03:34 +0200

    added ranking and tsearch for partial words with pg search gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,12 @@ class Article < ActiveRecord::Base
   validates_format_of :source_link, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
   include PgSearch::Model
   pg_search_scope :search_by_title_and_body,
-                  :against => [:title, :body]
+                  against: {
+                    title: 'A',
+                    body: 'B'
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  },
+                  ranked_by: ":tsearch / 2.0"
 end
